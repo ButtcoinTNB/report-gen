f621e4d5518fa4a71e4786d1c10f1d35531cbb98
ButtcoinTNB <bbuttcoin@gmail.com>
2025-03-16 12:04:00 +0100

    Add complete favicon support with site.webmanifest for better cross-platform experience

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -6,7 +6,14 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="description" content="Scrittore Automatico di Perizie - Create formatted reports from case documents" />
+        
+        {/* Favicon links for better cross-platform support */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+        
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
